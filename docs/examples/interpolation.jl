71422726900d4dfe2703b10d6bd74052cfc5df42
jondea <jondea@thedeakin.net>
2022-02-14 22:09:05 +0000

    Add rewrite of zeta interpolation in notebook

--- docs/examples/interpolation.jl
+++ docs/examples/interpolation.jl
@@ -64,7 +64,7 @@ k = 1.0
 md"""
 and a field, which will consist of two radial Fourier modes chosen so that there is a pole near to our transformation.
 Indeed so near that it ends up crossing it and creating a rip.
-We do this so that we can properly test out our interpolation and integration schemes on a difficult example. 
+We do this so that we can properly test out our interpolation and integration schemes on a difficult example.
 """
 
 # ╔═╡ 0d75f7cb-6503-412d-b4c7-76772d05ff33
@@ -72,18 +72,6 @@ u = let
 	a = two_mode_pole_coef(0:1, 2.0+1.0im)
 	HankelSeries(k, a)
 end
-
-# ╔═╡ 3c75cc96-a8d8-4f9d-b78d-3205108e1686
-md"## Find rips"
-
-# ╔═╡ 611b863a-796f-4b42-8eab-f4b31888e140
-rips = classify_outer_boundary(u, pml, 0, 2π; Nζ=101, ε=1e-5, δ=1e-1, verbose=false)
-
-# ╔═╡ 6f70d51b-220c-4e86-a96d-8432c6ed113c
-OptimalPMLTransformations.pole_newton_solve(u, pml, 0.350885, 6.15284, -0.474182+0.482679im)
-
-# ╔═╡ 01ba5c71-ba6e-4909-a9da-aa51ba591851
-import OptimalPMLTransformations.pole_newton_solve
 
 # ╔═╡ d8c6957a-8cbd-422b-b77c-4f842b7238d2
 md"""
@@ -102,34 +90,15 @@ So we use these at each point to construct a cubic Hermite interpolation.
 The plot below shows the points where the transformation has been evaluated, the solid line is the cubic Hermite interpolation, and the dashed line is a piecewise linear interpolation.
 """
 
-# ╔═╡ 429d80ae-7fcf-4240-a6f0-e955ba158b3b
-let	
-	ν_vec = Float64[]
-	tν_vec = ComplexF64[]
-	∂tν_∂ν_vec = ComplexF64[]
-	
-	ν_max = 0.999
-	θ = rips[2].ζ+0.001
-	optimal_pml_transformation_solve(u, pml, ν_max, θ, ν_vec, tν_vec, ∂tν_∂ν_vec)
-	intrp = CubicHermiteSplineInterpolation(ν_vec, tν_vec, ∂tν_∂ν_vec)
-	
-	ν_vec_plot = 0:0.001:ν_max
-	complex2cols(c) = [real.(c) imag.(c)]
-	
-	colors=[:orange :blue]
-	
-	plot(legend=false)
-	plot!(ν_vec, complex2cols(tν_vec), label=["real(linear approx)" "imag(linear approx)"], color=colors, linestyle=:dash)
-	plot!(ν_vec_plot, complex2cols(intrp.(ν_vec_plot)), label=["real(hermite approx)" "imag(hermite approx)"], color=colors)
-	scatter!(ν_vec, complex2cols(intrp.(ν_vec)), label=["real(knot)" "imag(knot)"], color=colors)
-end
-
 # ╔═╡ df0c1768-feb6-4c9d-9c3b-cd06781e6ab0
 md"""
 The transformation can be unbounded, specifically at the outer edge.
 Furthermore, the derivatives of the transformation can be unbounded at the tip of the rip.
 When determining the optimal transformation, if either quanties cannot be determined, we mark them as NaN.
 """
+
+# ╔═╡ 3c75cc96-a8d8-4f9d-b78d-3205108e1686
+md"## Find rips"
 
 # ╔═╡ 5a99aa34-151f-40b1-95b5-eba87ee4d5fb
 md"""
@@ -179,7 +148,7 @@ gausslegendre(5)
 
 # ╔═╡ 815b73d9-fa9a-4d4e-97ad-5daf1b8da5d9
 md"""
-## Technical bits 
+## Technical bits
 """
 
 # ╔═╡ 6a9a44c6-83e0-4756-941b-7e271756d4f3
@@ -211,7 +180,7 @@ md"## To go in library"
 
 # ╔═╡ b1d5ef17-7a3e-4a8e-8387-a04d75018fa0
 #Do not export
-function interpolant(PMLFieldFunction, ζ::AbstractVector)
+function interpolation_line(PMLFieldFunction, ζ::AbstractVector)
 end
 
 # ╔═╡ 404da5d5-737d-4df9-b3e1-6fac3038eb5c
@@ -226,22 +195,12 @@ end
 # ╔═╡ 8e497f14-8f77-45b9-8843-bc65e20a61d5
 u_pml = PMLFieldFunction(u, pml)
 
-# ╔═╡ 535dc5ec-506a-4afe-9d44-7657deb852f8
-#Do not export
-function interpolant(PMLFieldFunction, ζ::Number; ν_max=1.0)
-	νs = Float64[]
-	tνs = ComplexF64[]
-	∂tν_∂νs = ComplexF64[]
-	∂tν_∂ζs = ComplexF64[]
-	optimal_pml_transformation_solve(u_pml.u, u_pml.pml, ν_max, ζ, νs, tνs, ∂tν_∂νs, ∂tν_∂ζs; silent_failure=true)
-	return InterpLine(ζ, νs, tνs, ∂tν_∂νs, ∂tν_∂ζs)
-end
-
 # ╔═╡ ec66435f-aeb2-472e-91f4-d510d801cdf0
+# "Use Newton's method to find a single rip from an initial guess"
 function find_rip(u_pml::PMLFieldFunction, ν::Real, ζ::Real, tν::Number; ε=1e-12)
 
     x = SA[ν, ζ, real(tν), imag(tν)]
-	
+
 	U, ∂U_∂tζ = u_pml(NamedTuple{(:u, :∂u_∂tζ)}, 0.0+0.0im, ζ)
 	(;u, ∂u_∂tν, ∂u_∂tζ, ∂2u_∂tν2, ∂2u_∂tν∂tζ) = u_pml(NamedTuple{(:u, :∂u_∂tν, :∂u_∂tζ, :∂2u_∂tν2, :∂2u_∂tν∂tζ)}, tν, ζ)
 
@@ -249,7 +208,7 @@ function find_rip(u_pml::PMLFieldFunction, ν::Real, ζ::Real, tν::Number; ε=1
 	o_rip = ∂u_∂tν
 	o_opt = u - U*(1-ν)
     r = SA[real(o_rip), imag(o_rip), real(o_opt), imag(o_opt)]
-	
+
     n_iter = 1
     while maximum(abs.(r)) > ε*abs(U) && n_iter < 100
         # Create Jacobian of objectives and unknowns
@@ -279,7 +238,18 @@ function find_rip(u_pml::PMLFieldFunction, ν::Real, ζ::Real, tν::Number; ε=1
         n_iter += 1
     end
 
-    return ν, ζ, tν
+    return Rip2D(ζ, ν, tν)
+end
+
+# ╔═╡ 535dc5ec-506a-4afe-9d44-7657deb852f8
+#Do not export
+function interpolation_line(u_pml::PMLFieldFunction, ζ::Number; ν_max=1.0)
+	νs = Float64[]
+	tνs = ComplexF64[]
+	∂tν_∂νs = ComplexF64[]
+	∂tν_∂ζs = ComplexF64[]
+	optimal_pml_transformation_solve(u_pml.u, u_pml.pml, ν_max, ζ, νs, tνs, ∂tν_∂νs, ∂tν_∂ζs; silent_failure=true)
+	return InterpLine(ζ, νs, tνs, ∂tν_∂νs, ∂tν_∂ζs)
 end
 
 # ╔═╡ 02105c89-d058-4cea-ae12-0481eacb6fdc
@@ -293,6 +263,24 @@ begin
 		f2, i2 = findmax(f, l2.points)
 		f1 < f2 ? (l1, l1.points[i1]) : (l2, l2.points[i2])
 	end
+end
+
+# ╔═╡ 4ca27bec-49df-4eb5-b60c-5d7bbfb56c75
+# "Use Newton's method to find a single rip somewhere between two lines"
+function find_rip(u_pml::PMLFieldFunction, tν₋::InterpLine, tν₊::InterpLine)
+	# Find mostly likely point for rip along the two lines to use as an initial guess
+	p₋ = argmax(p->abs((1-p.ν)*p.∂tν_∂ν), tν₋.points)
+	p₊ = argmax(p->abs((1-p.ν)*p.∂tν_∂ν), tν₊.points)
+	ζ₋ = tν₋.ζ
+	ζ₊ = tν₊.ζ
+	νᵣ = (p₋.ν + p₊.ν)/2
+	tνᵣ = (p₋.tν + p₊.tν)/2
+	ζᵣ = (ζ₊ + ζ₋)/2
+	rip = find_rip(u_pml, νᵣ, ζᵣ, tνᵣ)
+	if !(ζ₋ <= ζᵣ <= ζ₊)
+		@warn "ζ from find_rip was outside of range $ζ₋ $ζᵣ $ζ₊"
+	end
+	rip
 end
 
 # ╔═╡ 973df994-bb15-4681-a6e6-bde44766c046
@@ -309,33 +297,25 @@ function relative_l2_difference(line1::InterpLine, line2::InterpLine)
 end
 
 # ╔═╡ a74672a6-008a-43bc-b07e-1581064f3d02
-function find_rips!(rips::Vector{Rip2D}, u_pml::PMLFieldFunction, ζs::AbstractVector; ε=1e-1, δ=1e-8, tν_metric=relative_l2_difference)::Vector{Rip2D}
+"Find all rips of size >ε between ζs, by recursively subdividing"
+function find_rips!(rips::Vector{Rip2D}, u_pml::PMLFieldFunction, ζs::AbstractVector;
+	ε=1e-1, δ=1e-8, tν_metric=relative_l2_difference,
+	tν₋ = interpolation_line(u_pml, first(ζs)), tν₊ = interpolation_line(u_pml, last(ζs)),)::Vector{Rip2D}
 
-	tν₋ = interpolant(u_pml, first(ζs))
-	for (ζ₋, ζ₊) in consecutive_pairs(ζs)
-		tν₊ = interpolant(u_pml, ζ₊)
-		if tν_metric(tν₋,tν₊) > ε
-			if abs(ζ₊ - ζ₋) < δ
-				# Find mostly likely point for rip along the two lines to use as an initial guess
-				p₋ = argmax(p->abs((1-p.ν)*p.∂tν_∂ν), tν₋.points)
-				p₊ = argmax(p->abs((1-p.ν)*p.∂tν_∂ν), tν₊.points)
-				νᵣ = (p₋.ν + p₊.ν)/2
-				tνᵣ = (p₋.tν + p₊.tν)/2
-				ζᵣ = (ζ₊ + ζ₋)/2
-				νᵣ, ζᵣ, tνᵣ = find_rip(u_pml, νᵣ, ζᵣ, tνᵣ)
-				if !(ζ₋ <= ζᵣ <= ζ₊)
-					@warn "ζ from find_rip was outside of range $ζ₋ $ζᵣ $ζ₊"
-				end
-				push!(rips, Rip2D(ζᵣ, νᵣ, tνᵣ))
+	tν₁ = tν₋
+	for (ζ₁, ζ₂) in consecutive_pairs(ζs)
+		tν₂ = ζ₂ == last(ζs) ? tν₊ : interpolation_line(u_pml, ζ₂)
+		if tν_metric(tν₁,tν₂) > ε
+			if abs(ζ₂ - ζ₁) < δ
+				push!(rips, find_rip(u_pml, tν₁, tν₂))
 			else
-				find_rips!(rips, u_pml, range(ζ₋, ζ₊, length=3); ε, δ)
+				find_rips!(rips, u_pml, range(ζ₁, ζ₂, length=3); ε, δ, tν₋=tν₁, tν₊=tν₂)
 			end
 		end
-		tν₋ = tν₊
+		tν₁ = tν₂
 	end
-	
-    return rips
 
+    return rips
 end
 
 # ╔═╡ d68dbba7-eac0-44c2-af0c-08fa5638e954
@@ -345,7 +325,89 @@ function find_rips(args...; kwargs...)::Vector{Rip2D}
 end
 
 # ╔═╡ 92a2f127-bdaa-4ab9-90f3-68c2b6b0fc49
-find_rips(u_pml, range(0, τ, length=5))
+rips = find_rips(u_pml, range(0, τ, length=11))
+
+# ╔═╡ 429d80ae-7fcf-4240-a6f0-e955ba158b3b
+let
+	ν_vec = Float64[]
+	tν_vec = ComplexF64[]
+	∂tν_∂ν_vec = ComplexF64[]
+
+	ν_max = 0.999
+	θ = rips[2].ζ+0.001
+	optimal_pml_transformation_solve(u, pml, ν_max, θ, ν_vec, tν_vec, ∂tν_∂ν_vec)
+	intrp = CubicHermiteSplineInterpolation(ν_vec, tν_vec, ∂tν_∂ν_vec)
+
+	ν_vec_plot = 0:0.001:ν_max
+	complex2cols(c) = [real.(c) imag.(c)]
+
+	colors=[:orange :blue]
+
+	plot(legend=false)
+	plot!(ν_vec, complex2cols(tν_vec), label=["real(linear approx)" "imag(linear approx)"], color=colors, linestyle=:dash)
+	plot!(ν_vec_plot, complex2cols(intrp.(ν_vec_plot)), label=["real(hermite approx)" "imag(hermite approx)"], color=colors)
+	scatter!(ν_vec, complex2cols(intrp.(ν_vec)), label=["real(knot)" "imag(knot)"], color=colors)
+end
+
+# ╔═╡ 3014f1ba-3bce-43a2-bf84-e22d08aabd38
+function adaptively_append!(intrp::Interpolation, u_pml::PMLFieldFunction, ζs::AbstractVector; ε=1e-1, δ=1e-8, tν_metric=relative_l2_difference, tν₊=interpolation_line(u_pml, last(ζs)))::Vector{Rip2D}
+
+	tν₁ = last(last(interp.continous_region).lines)
+	for (ζ₁, ζ₂) in consecutive_pairs(ζs)
+		tν₂ = (ζ₂ == ζ₊) ? tν₊ : interpolation_line(u_pml, ζ₂)
+		if tν_metric(tν₁,tν₂) > ε
+			if abs(ζ₂ - ζ₁) < δ
+				rip = find_rip(u_pml, tν₁, tν₂)
+
+				push!(intrp, tν₁)
+				
+				# Add line on rip, continued from below
+				tν_rip₋ = continue_in_ζ(u_pml, ζ_rip, tν₁)
+				insertsorted!(tν_rip₋.points, rip; by=p->p.ν)
+				push!(intrp, tν_rip₋)
+
+				# Adding a rip severs the two domains
+				push!(intrp, rip)
+				
+				# Add line on rip, continued from above
+				tν_rip₊ = continue_in_ζ(u_pml, ζ_rip, tν₂)
+				insertsorted!(tν_rip₊.points, rip; by=p->p.ν)
+				push!(intrp, tν_rip₊)
+
+				push!(intrp, tν₂)
+
+				return
+			else
+				# Recursively split domain into 2 regions (3 points with ends)
+				adaptively_append!(intrp, u_pml, range(ζ₁, ζ₂, length=3); ε, δ)
+			end
+		end
+		tν₁ = tν₂
+	end
+
+	return intrp
+end
+
+# ╔═╡ d893b9d4-08ac-4693-8016-e0b857c07dba
+"""
+Find interpolation of `u_pml` at all ζs, adapting in ν and ζ
+
+Subdivide in ζ if the tν_metric between two consecutive lines is >ε.
+Add a rip if the division in ζ is <δ.
+"""
+function interpolation(u_pml::PMLFieldFunction, ζs::AbstractVector; kwargs...)::Interpolation
+
+	intrp = Interpolation()
+	tν₋ = interpolation(u_pml, first(ζs))
+	push!(intrp, tν₋)
+
+	tν₊ = interpolation(u_pml, last(ζs))
+	adaptively_append!(intrp, ζs; tν₊, kwargs...)
+	push!(intrp, tν₊)
+
+	return intrp
+
+end
 
 # ╔═╡ 46ae12d1-593c-4eef-a324-ae53f3192896
 function relative_max_difference(line1::InterpLine, line2::InterpLine)
@@ -367,7 +429,13 @@ rip_objective(field::Union{NamedTuple,InterpPoint}) = field.∂u_∂tν
 optimal_transformation_objective(u, U, ν) = u - U*(1-ν)
 
 # ╔═╡ f4adc5b8-6dfd-45d1-afd9-f5002f134b0b
-
+html"""
+<style>
+  main {
+    max-width: 900px;
+  }
+</style>
+"""
 
 # ╔═╡ Cell order:
 # ╟─2dcb0cb8-0d06-4d55-9bf9-ed27b5c5bc7e
@@ -380,18 +448,18 @@ optimal_transformation_objective(u, U, ν) = u - U*(1-ν)
 # ╟─2a717cb2-82e1-4470-b871-9dea2bd70e2c
 # ╠═0d75f7cb-6503-412d-b4c7-76772d05ff33
 # ╠═8e497f14-8f77-45b9-8843-bc65e20a61d5
-# ╟─3c75cc96-a8d8-4f9d-b78d-3205108e1686
-# ╠═611b863a-796f-4b42-8eab-f4b31888e140
-# ╠═6f70d51b-220c-4e86-a96d-8432c6ed113c
-# ╠═92a2f127-bdaa-4ab9-90f3-68c2b6b0fc49
-# ╠═a74672a6-008a-43bc-b07e-1581064f3d02
-# ╠═01ba5c71-ba6e-4909-a9da-aa51ba591851
-# ╠═d68dbba7-eac0-44c2-af0c-08fa5638e954
-# ╠═ec66435f-aeb2-472e-91f4-d510d801cdf0
 # ╟─d8c6957a-8cbd-422b-b77c-4f842b7238d2
 # ╠═429d80ae-7fcf-4240-a6f0-e955ba158b3b
 # ╟─df0c1768-feb6-4c9d-9c3b-cd06781e6ab0
-# ╠═5a99aa34-151f-40b1-95b5-eba87ee4d5fb
+# ╟─3c75cc96-a8d8-4f9d-b78d-3205108e1686
+# ╠═92a2f127-bdaa-4ab9-90f3-68c2b6b0fc49
+# ╠═a74672a6-008a-43bc-b07e-1581064f3d02
+# ╠═d68dbba7-eac0-44c2-af0c-08fa5638e954
+# ╠═4ca27bec-49df-4eb5-b60c-5d7bbfb56c75
+# ╠═ec66435f-aeb2-472e-91f4-d510d801cdf0
+# ╟─5a99aa34-151f-40b1-95b5-eba87ee4d5fb
+# ╠═d893b9d4-08ac-4693-8016-e0b857c07dba
+# ╠═3014f1ba-3bce-43a2-bf84-e22d08aabd38
 # ╟─f5faa6af-9bb2-41ec-9517-579fc41db3e2
 # ╠═53a2dc51-6684-468d-b95a-6fea0937269e
 # ╠═e57e4592-4e66-45ca-b0ad-7fd32e1bdd5d
