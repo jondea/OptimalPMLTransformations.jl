8da26f8e0be58607f5f0f9d3f4420491f1318865
jondea <jondea@thedeakin.net>
2022-09-02 23:14:38 +0100

    Add tr and jacobian function for InvHankelSeries

--- src/transformations/inv_hankel_series.jl
+++ src/transformations/inv_hankel_series.jl
@@ -14,10 +14,20 @@ mutable struct InvHankelSeriesPML{P <: AnnularPML, S <: HankelSeries}
     interp::Interpolation
 end
 
+InvHankelSeriesPML(geom, u) = InvHankelSeriesPML(geom, u, Interpolation())
+
 # Note, we can set the default range to [0,τ] because an Annular PML is a full turn.
 # ν_max=1 means we attempt to set the whole PML region
-function add_interpolation!(p::InvHankelSeriesPML; ζs = range(0.0, τ, length=11), ν_max=1.0, kwargs...)
+function add_interpolation!(p::InvHankelSeriesPML; num_ζs=11, ζs = range(0.0, τ, length=num_ζs), ν_max=1.0, kwargs...)
     p.interp = interpolate(p.u, p.pml, ζs, ν_max; kwargs...)
 end
 
-has_interpolation(p::InvHankelSeriesPML) = !isempty(p.interp.continuous_region)
+has_interpolation(p::InvHankelSeriesPML) = !isempty(p.interp)
+
+function tr_and_jacobian(pml::InvHankelSeriesPML, patch::InterpPatch, coords::PolarCoordinates)
+    return tr_and_jacobian(pml, patch, convert(PMLCoordinates, coords, pml.geom))
+end
+
+function tr_and_jacobian(pml::InvHankelSeriesPML, patch::InterpPatch, coords::PMLCoordinates)
+    return evalute_and_correct(pml.u, pml.geom, patch, coords.ν, coords.ζ)
+end
