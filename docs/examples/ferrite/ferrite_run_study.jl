ec8fce8f3c923091cf261eb1888c204f4031282c
jondea <jondea@thedeakin.net>
2022-11-26 21:49:47 +0000

    Remove notebook stuff from ferrite_run_stdy.jl

--- docs/examples/ferrite/ferrite_run_study.jl
+++ docs/examples/ferrite/ferrite_run_study.jl
@@ -1,10 +1,4 @@
-### A Pluto.jl notebook ###
-# v0.19.11
 
-using Markdown
-using InteractiveUtils
-
-# ╔═╡ a51bf9fd-11a6-48ba-9b0d-ab4397be015c
 begin
     import Pkg
     # activate the shared project environment
@@ -29,29 +23,15 @@ begin
     using DataFrames
 end
 
-# ╔═╡ c80b6d31-ba85-4d8a-bc28-20dde3f74985
 @revise using Ferrite
 
-# ╔═╡ b3e4776d-8d03-47c2-8057-8b6e4f443b90
 @revise using OptimalPMLTransformations
 
-# ╔═╡ 424b9b91-9926-4c54-9f9d-329c974a8336
 @revise using PMLFerriteExtensions
 
-# ╔═╡ 4c152d80-c523-4da1-9035-7d7b4cb144d9
-md"# PML Helmholtz equation in annulus in polar coordinates"
-
-# ╔═╡ 1d9ac613-63c0-4c34-8c84-1af02a5f4172
-md"## Parameters"
-
-# ╔═╡ c75d6e53-ee17-4587-925c-f253705b633d
 # Single case for debugging
 # solve_and_save(;k=0.1, N_θ=3, N_r=1, N_pml=1, cylinder_radius=1.0, R=2.0, δ_pml=1.0, n_h=3, order=2, folder=tempname("./"))
 
-# ╔═╡ 0d050dc4-0b97-4a0c-b399-970e1f7284c9
-md"## Constraints"
-
-# ╔═╡ 53ae2bac-59b3-44e6-9d6c-53a18567ea2f
 function setup_constraint_handler(dh::Ferrite.AbstractDofHandler, left_bc, right_bc)
 	ch = ConstraintHandler(dh)
 
@@ -74,55 +54,22 @@ function setup_constraint_handler(dh::Ferrite.AbstractDofHandler, left_bc, right
 	ch
 end
 
-# ╔═╡ 30826935-619b-460a-afc2-872950052f7a
-md"## Assembly"
-
-# ╔═╡ 8296d8a7-41ce-4111-9564-05e7cdc4bfe8
-md"## Solve and plot"
-
-# ╔═╡ 27d3071a-722e-4c0d-98b9-cd04d7c7980a
-md"# Appendix"
-
-# ╔═╡ 9cfd1f97-f333-40a7-98db-cb9373a857a0
-md"## Utils"
-
-# ╔═╡ b5b526dd-37ef-484c-97fd-2305f0d1d714
-html"""
-<style>
-  main {
-    max-width: 900px;
-  }
-</style>
-"""
-
-# ╔═╡ 059eceb1-39fd-4a5e-a787-23d2bbf9b547
-PlutoUI.TableOfContents()
-
-# ╔═╡ 759eb280-f138-46f8-af4e-7447a38c4a9f
 (f::AbstractFieldFunction)(c::Node) = f(c.x)
 
-# ╔═╡ 424be202-5f13-46e1-a4db-8cbd6937fdcc
 # Ignore time dependence (problem is time harmonic)
 (f::AbstractFieldFunction)(x::Vec{2}, _t::Number) = f(x)
 
-# ╔═╡ ea07bf0c-552c-46fe-9431-4ca6a99e3647
 # Interpret Vec (the problem coordinates) as polar coordinates for the field function
 (f::AbstractFieldFunction)(x::Vec{2}) = f(PolarCoordinates(x[1],x[2]))
 
-# ╔═╡ 691593a3-4e68-4b3c-8020-ee07bf773486
 Base.in(n::Node, pml::PMLGeometry) = in(n.x, pml)
 
-# ╔═╡ 8947ed4c-ac37-4531-9252-63b195b73577
 Base.in(x::AbstractVector, pml::AnnularPML) = x[1] >= pml.R
 
-# ╔═╡ 7fca1678-2d50-4ade-be8b-43430f751fcf
 Base.in(x::AbstractVector, pml::SFB) = in(x, pml.geom)
 
-# ╔═╡ 04a4723c-c9d3-46cd-8d22-48b9c992f366
 Base.in(x::AbstractVector, pml::PML) = in(x, pml.geom)
 
-
-# ╔═╡ baca19e6-ee39-479e-9bd5-f5838cc9f869
 function doassemble(cellvalues::CellScalarValues{dim}, pml_cellvalues::CellScalarValues{dim},
                          K::SparseMatrixCSC, dh::DofHandler, pml, k::Number) where {dim}
     T = dof_type(dh)
@@ -195,7 +142,6 @@ end
 
 consecutive_pairs(r) = IterTools.partition(r, 2, 1)
 
-# ╔═╡ 721a855f-49aa-40ae-93ad-a1cc5313e399
 function doassemble(cellvalues::CellScalarValues{dim}, K::SparseMatrixCSC, dh::DofHandler, pml::InvHankelSeriesPML, k::Number) where {dim}
 
     T = dof_type(dh)
@@ -351,7 +297,6 @@ function doassemble(cellvalues::CellScalarValues{dim}, K::SparseMatrixCSC, dh::D
     return K, f
 end
 
-# ╔═╡ 5ec5d21a-3bcf-4cde-a814-f4fb71c30a2f
 function solve_for_error(;k, N_θ, N_r, N_pml, cylinder_radius=1.0, R=2.0, δ_pml=1.0, u_ana, order=2, pml, qr::QuadratureRule, pml_qr::QuadratureRule)
 	# Set to NaN initially in case we throw
 	(assemble_time, solve_time, abs_sq_error, abs_sq_norm, rel_error) = Iterators.cycle(NaN)
@@ -415,7 +360,6 @@ let
     @show (assemble_time, solve_time, abs_sq_error, abs_sq_norm, rel_error)
 end
 
-# ╔═╡ ef1dac4e-77ea-47ef-a94a-bf63395120bc
 function solve_and_save(;k, N_θ, N_r, N_pml, cylinder_radius=1.0, R=2.0, δ_pml=1.0, n_h, order=2, folder)
 
 	nqr_1d = 2*(order + 1)
@@ -464,7 +408,6 @@ function solve_and_save(;k, N_θ, N_r, N_pml, cylinder_radius=1.0, R=2.0, δ_pml
 	end
 end
 
-# ╔═╡ 4770eea5-ddab-4d98-90b9-4f95fec8a23e
 function run_all(;test_run=false)
 	folder=tempname("./")
 
@@ -490,13 +433,10 @@ function run_all(;test_run=false)
     CSV.read("$folder/result.csv", DataFrame)
 end
 
-# ╔═╡ 76070b3b-2663-481f-8deb-cf995bb9b640
 # results_df = run_all(test_run=true)
 
-# ╔═╡ 1d6054f2-6ae5-4d09-8c7e-fe536512e3c6
 # filter(r->r.N_pml == 1, results_df);
 
-# ╔═╡ d87c0552-eec5-4765-ad04-03fbab2727fe
 function solve(ch, cellvalues, pml_cellvalues)
 	K = create_sparsity_pattern(ch.dh, ch)
 	K, f = doassemble(cellvalues, pml_cellvalues, K, ch.dh)
@@ -505,39 +445,3 @@ function solve(ch, cellvalues, pml_cellvalues)
 	apply!(u, ch)
 	return u
 end
-
-# ╔═╡ 45be7b0b-c64d-4cfe-a399-de62e6b9094e
-md"## Dependencies"
-
-# ╔═╡ Cell order:
-# ╟─4c152d80-c523-4da1-9035-7d7b4cb144d9
-# ╟─1d9ac613-63c0-4c34-8c84-1af02a5f4172
-# ╠═76070b3b-2663-481f-8deb-cf995bb9b640
-# ╠═1d6054f2-6ae5-4d09-8c7e-fe536512e3c6
-# ╠═c75d6e53-ee17-4587-925c-f253705b633d
-# ╠═4770eea5-ddab-4d98-90b9-4f95fec8a23e
-# ╠═5ec5d21a-3bcf-4cde-a814-f4fb71c30a2f
-# ╠═ef1dac4e-77ea-47ef-a94a-bf63395120bc
-# ╟─0d050dc4-0b97-4a0c-b399-970e1f7284c9
-# ╠═53ae2bac-59b3-44e6-9d6c-53a18567ea2f
-# ╟─30826935-619b-460a-afc2-872950052f7a
-# ╠═baca19e6-ee39-479e-9bd5-f5838cc9f869
-# ╠═721a855f-49aa-40ae-93ad-a1cc5313e399
-# ╟─8296d8a7-41ce-4111-9564-05e7cdc4bfe8
-# ╠═d87c0552-eec5-4765-ad04-03fbab2727fe
-# ╟─27d3071a-722e-4c0d-98b9-cd04d7c7980a
-# ╟─9cfd1f97-f333-40a7-98db-cb9373a857a0
-# ╠═b5b526dd-37ef-484c-97fd-2305f0d1d714
-# ╠═059eceb1-39fd-4a5e-a787-23d2bbf9b547
-# ╠═759eb280-f138-46f8-af4e-7447a38c4a9f
-# ╠═424be202-5f13-46e1-a4db-8cbd6937fdcc
-# ╠═ea07bf0c-552c-46fe-9431-4ca6a99e3647
-# ╠═691593a3-4e68-4b3c-8020-ee07bf773486
-# ╠═8947ed4c-ac37-4531-9252-63b195b73577
-# ╠═7fca1678-2d50-4ade-be8b-43430f751fcf
-# ╠═04a4723c-c9d3-46cd-8d22-48b9c992f366
-# ╟─45be7b0b-c64d-4cfe-a399-de62e6b9094e
-# ╠═a51bf9fd-11a6-48ba-9b0d-ab4397be015c
-# ╠═c80b6d31-ba85-4d8a-bc28-20dde3f74985
-# ╠═b3e4776d-8d03-47c2-8057-8b6e4f443b90
-# ╟─424b9b91-9926-4c54-9f9d-329c974a8336
