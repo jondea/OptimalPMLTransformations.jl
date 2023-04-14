3bfc7f8f71cf888b39fee549ea1fb5a25611a5ef
jondea <jondea@thedeakin.net>
2023-04-14 21:08:27 +0100

    Small improvements to ferrite debug example

--- docs/examples/ferrite/ferrite_debug.jl
+++ docs/examples/ferrite/ferrite_debug.jl
@@ -35,20 +35,16 @@ N_pml = 0
 N_r = 2
 
 int_order = 2
-δ_pml=1.0
+δ_pml = 1.0
 cylinder_radius = 1.0
 nqr_1d = 2*(int_order + 1)
-bulk_qr=QuadratureRule{2, RefCube}(nqr_1d)
-pml_qr=QuadratureRule{2, RefCube}(nqr_1d)
+bulk_qr = QuadratureRule{2, RefCube}(nqr_1d)
+pml_qr = QuadratureRule{2, RefCube}(nqr_1d)
 # u_ana=HankelSeries(k, OffsetVector([1.0], n_h:n_h))
 # u_ana=two_mode_pole_series(k, R + (1.0+1.0im))
-a = scattered_coef(-10:10, k)
-u_ana = HankelSeries(k, a)
+u_ana = HankelSeries(k, scattered_coef(-10:10, k))
 
 pml = InvHankelSeriesPML(AnnularPML(R, δ_pml), u_ana)
-result = solve(PMLHelmholtzPolarAnnulusParams(;k, N_θ, N_r, N_pml, cylinder_radius, R, δ_pml, u_ana, order=int_order, pml, bulk_qr, pml_qr))
-@show result
+u_pml = PMLFieldFunction(u_ana, pml.geom)
 
-# (θ_min, θ_max) = (0.0, τ)
-# intrp = interpolation(PMLFieldFunction(pml), range(θ_min, θ_max, length=21))
-
+solve(PMLHelmholtzPolarAnnulusParams(;k, N_θ, N_r, N_pml, cylinder_radius, R, δ_pml, u_ana, order=int_order, pml, bulk_qr, pml_qr); save_vtk=true)
